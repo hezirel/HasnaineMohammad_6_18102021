6630089f9c66dcd7342b6a115b5bd331bfb99db2
hezirel <hezirel@gmail.com>
2021-10-20 18:27:12 +0200

    var refactoring

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ const data = await fetch("./data.json").then(res => res.json())
 //#:Add error handler .then or .catch function => display http error
 
 // PhOTOGRAPHERS DATA
-let photographerId = ""
-let photographerName = ""
-let photographerCity = ""
-let photographerCountry = ""
-let photographerTagline = ""
-let photographerPrice = ""
-let photographerPortrait = ""
 let photographerTags = ""
 var tagsList = [];
 
@@ -19,34 +12,32 @@ var tagsList = [];
 const photographers = data["photographers"]
 
 photographers.forEach((photographer, index) => {
-    photographerId = photographer["id"]
-    photographerName = photographer["name"]
-    photographerCity = photographer["city"]
-    photographerCountry = photographer["country"]
-    photographerTagline = photographer["tagline"]
-    photographerPrice = photographer["price"]
-    photographerPortrait = photographer["portrait"]
     photographerTags = photographer["tags"]
     photographer["tags"].forEach((string) => {
         if (!tagsList.includes(string)) 
             tagsList.push(string);
     })
+    //#:change this to a appendchild later
     homeContainer.innerHTML += `
     <article class="user">
     <a href="">
-        <section class="user-view" data-id="${photographerId}" tabindex="10">
-            <img class="profile-pic" src="./images/Photographers ID Photos/${photographerPortrait}" alt="${photographerName}">
-            <h1 class="username">${photographerName}</h1>
+        <section class="user-view" data-id="${photographer.id}" tabindex="10">
+            <img class="profile-pic" src="./images/Photographers ID Photos/${photographer.portrait}" alt="${photographer.name}">
+            <h1 class="username">${photographer.name}</h1>
         </section>
     </a>
     <section class="user-info">
-        <p class="user-loc"><span class="city">${photographerCity}</span>, <span class="country">${photographerCountry}</span></p>
-        <p class="user-tagline">${photographerTagline}</p>
-        <p class="user-price"><span class="price">${photographerPrice}</span>€/jour</p>
+        <p class="user-loc"><span class="city">${photographer.city}</span>, <span class="country">${photographer.country}</span></p>
+        <p class="user-tagline">${photographer.tags}</p>
+        <p class="user-price"><span class="price">${photographer.price}</span>€/jour</p>
         <ul class="links tags"></ul>
     </section>
 </article>
     `
+    // Send html data to another function with if condition depending
+    //third parameter passed to foreach ?
+    //Then if photographer["tags"].has filters[*]
+    //append child
     // Display tags inside photographers
     const tags = document.querySelectorAll('.tags')
 
@@ -67,6 +58,7 @@ photographers.forEach((photographer, index) => {
         currentId = currentPhotographer.getAttribute('data-id')
     })
 })
+
 const menuBar = document.querySelector('.links');
 tagsList.forEach((uniqueTag, index) => {
     menuBar.innerHTML += `
